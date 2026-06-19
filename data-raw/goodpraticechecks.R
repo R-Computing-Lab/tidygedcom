cc92ea2286ff9c0a77f76ccfc40f521a5ce8542d
Mason Garrison <6001608+smasongarrison@users.noreply.github.com>
2026-06-18 23:48:23 -0400

    fixed?

--- data-raw/goodpraticechecks.R
+++ data-raw/goodpraticechecks.R
@@ -1,4 +1,10 @@
 library(goodpractice)
+checks_sans <- goodpractice::default_checks()[
+  !grepl("cyclocomp", goodpractice::default_checks())
+]
+checks_sans <- checks_sans[!grepl("rcmdcheck", checks_sans)]
 
 # Check the package
-gp <- goodpractice::gp()
+gp <- goodpractice::gp(
+checks = checks_sans
+)
